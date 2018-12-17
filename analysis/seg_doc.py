414e42d268e60e4c620a1134ac5db0e3ea0623ce
yanqiangmiffy <1185918903@qq.com>
2018-12-17 11:31:39 +0800

    gengxin

--- analysis/seg_doc.py
+++ analysis/seg_doc.py
@@ -6,7 +6,7 @@
 import jieba
 
 temp = []
-with open('sse_doc.txt', 'r', encoding='utf-8') as doc_file:
+with open('浙江台华新材料股份有限公司.txt', 'r', encoding='utf-8') as doc_file:
     for para in doc_file.read().split('。'):
         if '风险' in para and len(para) < 500:
             para=para.replace(' ','')
--- sse_download_pdf.py
+++ sse_download_pdf.py
@@ -18,6 +18,7 @@ def break_piont_continue(flag=True):  # 读取url
     :return:
     """
     df = pd.read_csv('data/sse.csv')
+    print(len(df))
     if flag:
         with open('log/sse_pdf_log.txt', 'r', encoding='utf-8') as log_file:
             fail_urls = [url.strip() for url in log_file.readlines()]
@@ -33,35 +34,36 @@ def break_piont_continue(flag=True):  # 读取url
         existed_files = os.listdir('sse_pdf/')
         file_names = df[~df['unique_names'].isin(existed_files)]['CTITLE_TXT'].tolist()
         download_urls = df[~df['unique_names'].isin(existed_files)]['CURL'].tolist()
-        print(len(existed_files))
+        print(len(file_names))
         return file_names, download_urls
 
+break_piont_continue(flag=False)
 
 # '''下载图片程序'''
-async def download_pics(name, url, semaphore):
-    async with semaphore:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                try:
-                    pdf = await response.read()  # 以Bytes方式读入非文字
-                    with open(folder_path + name + '_' + url.split('/')[-1], 'wb') as fout:  # 写入文件
-                        fout.write(pdf)
-                        print(name + '_' + url.split('/')[-1] + ' done!')
-                    # await asyncio.sleep(2)
-                except Exception as e:
-                    print(e)
-                    with open('log/sse_pdf_log.txt', 'a', encoding='utf-8') as log_file:
-                        log_file.write(url + '\n')
-
-
-if __name__ == '__main__':
-    file_names, download_urls = break_piont_continue(flag=False)
-    print(file_names, download_urls)
-    loop = asyncio.get_event_loop()  # 创建loop asyncio协程要在loop中运行
-    start_time = time.time()  # 用于统计时间
-    semaphore = asyncio.Semaphore(100)  # 限制并发量为500
-    tasks = [download_pics(name, url, semaphore) for name, url in zip(file_names, download_urls)]
-    loop.run_until_complete(asyncio.gather(*tasks))
-    loop.close()
-    end_time = time.time()
-    print(end_time - start_time)
+# async def download_pics(name, url, semaphore):
+#     async with semaphore:
+#         async with aiohttp.ClientSession() as session:
+#             async with session.get(url) as response:
+#                 try:
+#                     pdf = await response.read()  # 以Bytes方式读入非文字
+#                     with open(folder_path + name + '_' + url.split('/')[-1], 'wb') as fout:  # 写入文件
+#                         fout.write(pdf)
+#                         print(name + '_' + url.split('/')[-1] + ' done!')
+#                     # await asyncio.sleep(2)
+#                 except Exception as e:
+#                     print(e)
+#                     with open('log/sse_pdf_log.txt', 'a', encoding='utf-8') as log_file:
+#                         log_file.write(url + '\n')
+#
+#
+# if __name__ == '__main__':
+#     file_names, download_urls = break_piont_continue(flag=False)
+#     print(file_names, download_urls)
+#     loop = asyncio.get_event_loop()  # 创建loop asyncio协程要在loop中运行
+#     start_time = time.time()  # 用于统计时间
+#     semaphore = asyncio.Semaphore(100)  # 限制并发量为500
+#     tasks = [download_pics(name, url, semaphore) for name, url in zip(file_names, download_urls)]
+#     loop.run_until_complete(asyncio.gather(*tasks))
+#     loop.close()
+#     end_time = time.time()
+#     print(end_time - start_time)
