5f2f4527a43975737937a1373a6b0241fe3341cf
yanqiangmiffy <1185918903@qq.com>
2019-01-02 17:30:28 +0800

    风险更新

--- sse_main.py
+++ sse_main.py
@@ -9,9 +9,9 @@
 """
 import pandas as pd
 import jieba
+import re
 
 sse = pd.read_csv('data/sse.csv')
-szse = pd.read_csv('data/szse.csv')
 
 
 def gen_filename(row):
@@ -40,58 +40,77 @@ def get_text(filename):
 
 
 def count(txt):
-    print(txt)
+    #  风险词语个数
     risk_cnt = txt.count('风险')  # 风险词语个数
-
+    # 无风险个数
     without_risk_cnt = 0
     tag_word = ['无', '低', '小', '没有', '不高', '不大']
     for seg in txt.split('风险'):
         for word in tag_word:
             if word in seg[:5] or word in seg[-5:]:
                 without_risk_cnt += 1
+    # 文本字数
+    txt_len = len(txt)
 
-    txt_len = len(txt)  # 文本字数
+    # 风险部分统计
+    paras = count_risk_txtpara(txt)
+    if len(paras) != 0:
+        risk_txt_len = len("".join(paras))
+        temp = []
+        for para in "".join(paras).split('。'):
+            if 70 < len(para) < 500:
+                para = para.replace(' ', '')
+                temp.append(para)
+        risk_para_cnt = int(len(temp) // 1.5)
+        flag = "是"
+    else:
+        temp = []
+        for para in txt.split('。'):
+            if '风险' in para and 80 < len(para) < 500:
+                para = para.replace(' ', '')
+                temp.append(para)
+        risk_txt_len = len("".join(temp))  # 风险描述字数
+        risk_para_cnt = len(temp)  # 风险自然段落统计
+        flag = "否"
+    return risk_cnt, without_risk_cnt, txt_len, risk_txt_len, risk_para_cnt, flag
 
-    temp = []
-    for para in txt.split('。'):
-        if '风险' in para and len(para) < 500:
-            para = para.replace(' ', '')
-            temp.append(para)
-    risk_txt_len = int(len("".join(temp)) // 1.5)  # 风险描述字数
-    risk_para_cnt = int(len(temp) // 1.5)  # 风险自然段落统计
-    return risk_cnt, without_risk_cnt, txt_len, risk_txt_len, risk_para_cnt
+
+def count_risk_txtpara(txt):
+    paras = [para for para in re.split('第.节|第.条', txt) if '风险' in para[:20]]
+    paras = [para for para in paras if '...' not in para]
+    return paras
 
 
 sse['filename'] = sse.apply(lambda row: gen_filename(row), axis=1)
 sse['txt'] = sse.apply(lambda row: get_text(row.filename), axis=1)
 sse['risk_cnt'], sse['without_risk_cnt'], sse['txt_len'], sse['risk_txt_len'], sse[
-    'risk_para_cnt'] = zip(*sse['txt'].map(count))
-# from zhner.core import ner
-#
-# import numpy as np
-#
-#
-# def get_fullname(row):
-#     if row.CTITLE_TXT.count('公司') >=2:
-#         s = str(row.CTITLE_TXT)
-#     else:
-#         s = str(row.CONTENT)
-#     return ner(s).split('公司')[0]+'公司'
-#
-#
-# sse['full_name'] = sse.apply(lambda row: get_fullname(row), axis=1)
-#
-#
-# def get_subname(row):
-#     cans = [w for w in str(row.CONTENT).split(' ') if '股票简称' in w]
-#     if len(cans) != 0:
-#         return "".join(cans).replace('股票简称：', '')
-#     else:
-#         return np.nan
-#
-#
-# sse['sub_name'] = sse.apply(lambda row: get_subname(row), axis=1)
-#
-# cols = ['full_name', 'sub_name', 'CRELEASETIME', 'risk_cnt',
-#         'without_risk_cnt', 'txt_len', 'risk_txt_len', 'risk_para_cnt', 'filename']
-# sse[~sse['filename'].str.contains('摘要')].to_csv('result/sse_result.csv', index=False, header=False, columns=cols)
+    'risk_para_cnt'], sse['flag'] = zip(*sse['txt'].map(count))
+from zhner.core import ner
+
+import numpy as np
+
+
+def get_fullname(row):
+    if row.CTITLE_TXT.count('公司') >= 2:
+        s = str(row.CTITLE_TXT)
+    else:
+        s = str(row.CONTENT)
+    return ner(s).split('公司')[0] + '公司'
+
+
+sse['full_name'] = sse.apply(lambda row: get_fullname(row), axis=1)
+
+
+def get_subname(row):
+    cans = [w for w in str(row.CONTENT).split(' ') if '股票简称' in w]
+    if len(cans) != 0:
+        return "".join(cans).replace('股票简称：', '')
+    else:
+        return np.nan
+
+
+sse['sub_name'] = sse.apply(lambda row: get_subname(row), axis=1)
+
+cols = ['full_name', 'sub_name', 'CRELEASETIME', 'risk_cnt',
+        'without_risk_cnt', 'txt_len', 'risk_txt_len', 'risk_para_cnt', 'flag','filename']
+sse[~sse['filename'].str.contains('摘要')].to_csv('result/sse_result.csv', index=False, header=False, columns=cols)
--- szse_main.py
+++ szse_main.py
@@ -9,6 +9,7 @@
 """
 import pandas as pd
 import jieba
+import re
 
 szse = pd.read_csv('data/szse.csv')
 
@@ -39,64 +40,80 @@ def get_text(filename):
 
 
 def count(txt):
-    if '节风险因素' in txt:
-        print(txt.count('节风险因素'))
-        if txt.count('节风险因素')!=2:
-            print(txt)
+    #  风险词语个数
     risk_cnt = txt.count('风险')  # 风险词语个数
-
+    # 无风险个数
     without_risk_cnt = 0
     tag_word = ['无', '低', '小', '没有', '不高', '不大']
     for seg in txt.split('风险'):
         for word in tag_word:
             if word in seg[:5] or word in seg[-5:]:
                 without_risk_cnt += 1
+    # 文本字数
+    txt_len = len(txt)
 
-    txt_len = len(txt)  # 文本字数
+    # 风险部分统计
+    paras = count_risk_txtpara(txt)
+    if len(paras) != 0:
+        risk_txt_len = len("".join(paras))
+        temp = []
+        for para in "".join(paras).split('。'):
+            if 70 < len(para) < 500:
+                para = para.replace(' ', '')
+                temp.append(para)
+        risk_para_cnt = int(len(temp) // 1.5)
+        flag = "是"
+    else:
+        temp = []
+        for para in txt.split('。'):
+            if '风险' in para and 80 < len(para) < 500:
+                para = para.replace(' ', '')
+                temp.append(para)
+        risk_txt_len = len("".join(temp))  # 风险描述字数
+        risk_para_cnt = len(temp)  # 风险自然段落统计
+        flag = "否"
+    return risk_cnt, without_risk_cnt, txt_len, risk_txt_len, risk_para_cnt, flag
 
-    temp = []
-    for para in txt.split('。'):
-        if '风险' in para and len(para) < 500:
-            para = para.replace(' ', '')
-            temp.append(para)
-    risk_txt_len = int(len("".join(temp)) // 1.5)  # 风险描述字数
-    risk_para_cnt = int(len(temp) // 1.5)  # 风险自然段落统计
-    return risk_cnt, without_risk_cnt, txt_len, risk_txt_len, risk_para_cnt
+
+def count_risk_txtpara(txt):
+    paras = [para for para in re.split('第.节|第.条', txt) if '风险' in para[:10]]
+    paras = [para for para in paras if '...' not in para]
+    return paras
 
 
 szse['filename'] = szse.apply(lambda row: gen_filename(row), axis=1)
 szse['txt'] = szse.apply(lambda row: get_text(row.filename), axis=1)
 szse['risk_cnt'], szse['without_risk_cnt'], szse['txt_len'], szse['risk_txt_len'], szse[
-    'risk_para_cnt'] = zip(*szse['txt'].map(count))
-# from zhner.core import ner
-#
-# import numpy as np
-#
-#
-# def get_fullname(row):
-#     if row.doctitle.count('公司') >= 2:
-#         s = str(row.doctitle)
-#     else:
-#         s = str(row.doccontent)
-#     return ner(s).split('公司')[0] + '公司'
-#
-#
-# szse['full_name'] = szse.apply(lambda row: get_fullname(row), axis=1)
-#
-#
-# def get_subname(row):
-#     cans = [w for w in str(row.doctitle).split('：')]
-#     if len(cans) != 0:
-#         return cans[0]
-#     else:
-#         return np.nan
-#
-#
-# szse['sub_name'] = szse.apply(lambda row: get_subname(row), axis=1)
-#
-# cols = ['full_name', 'sub_name', 'docpubtime', 'risk_cnt',
-#         'without_risk_cnt', 'txt_len', 'risk_txt_len', 'risk_para_cnt', 'filename']
-# szse = szse[~szse['filename'].str.contains('摘要')]
-# szse = szse[~szse['filename'].str.contains('提示性公告')]
-#
-# szse.to_csv('result/szse_result.csv', index=False, header=False, columns=cols)
+    'risk_para_cnt'], szse['flag'] = zip(*szse['txt'].map(count))
+from zhner.core import ner
+
+import numpy as np
+
+
+def get_fullname(row):
+    if row.doctitle.count('公司') >= 2:
+        s = str(row.doctitle)
+    else:
+        s = str(row.doccontent)
+    return ner(s).split('公司')[0] + '公司'
+
+
+szse['full_name'] = szse.apply(lambda row: get_fullname(row), axis=1)
+
+
+def get_subname(row):
+    cans = [w for w in str(row.doctitle).split('：')]
+    if len(cans) != 0:
+        return cans[0]
+    else:
+        return np.nan
+
+
+szse['sub_name'] = szse.apply(lambda row: get_subname(row), axis=1)
+
+cols = ['full_name', 'sub_name', 'docpubtime', 'risk_cnt',
+        'without_risk_cnt', 'txt_len', 'risk_txt_len', 'risk_para_cnt', 'flag', 'filename']
+szse = szse[~szse['filename'].str.contains('摘要')]
+szse = szse[~szse['filename'].str.contains('提示性公告')]
+
+szse.to_csv('result/szse_result.csv', index=False, header=False, columns=cols)
